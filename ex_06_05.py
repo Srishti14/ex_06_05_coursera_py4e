6a40f77152eeaebf89507ed335f5288f58ff6a6f
Srishti Sinha <srishtisinha1402@gmail.com>
2018-03-17 17:30:36 +0530

    Update ex_06_05.py

--- ex_06_05.py
+++ ex_06_05.py
@@ -1,3 +1,9 @@
+'''Question: Take the following Python code that stores a string:
+str = ’X-DSPAM-Confidence:0.8475’
+Use find and string slicing to extract the portion of the string after the
+colon character and then use the float function to convert the extracted
+string into a floating point number.'''
+
 text = "X-DSPAM-Confidence:    0.8475";
 colon = text.find(":")
 number = text[colon+1:]
