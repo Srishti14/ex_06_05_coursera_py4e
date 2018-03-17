21d47529da3ac566bc6db2d47fa529d9b35a7270
Srishti Sinha <srishtisinha1402@gmail.com>
2018-03-17 17:40:02 +0530

    Update ex_06_05.py

--- ex_06_05.py
+++ ex_06_05.py
@@ -4,6 +4,7 @@ Use find and string slicing to extract the portion of the string after the
 colon character and then use the float function to convert the extracted
 string into a floating point number.'''
 
+
 text = "X-DSPAM-Confidence:    0.8475";
 colon = text.find(":")
 number = text[colon+1:]
